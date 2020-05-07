630a5c56990ae4b5f546a52cfb758a7bc0024811
bryandaniswara <bryan.daniswara@gmail.com>
2020-05-07 21:32:44 +0700

    Finished day 09 challenge

--- 09-Dev-Tools-Domination/main.js
+++ 09-Dev-Tools-Domination/main.js
@@ -10,25 +10,59 @@ function makeGreen() {
 }
 
 // Regular
+console.log('Hello');
 
 // Interpolated
+console.log('Hello %s!', 'Bryan');
 
 // Styled
+console.log('%c Styled text', 'font-size: 20px; color: blue');
 
 // warning!
+console.warn('Warning message!');
 
 // Error :|
+console.error('Error message!');
 
 // Info
+console.info('Info');
 
 // Testing
+const p = document.querySelector('p');
+
+console.assert(p.classList.contains('ouch'), 'That is wrong!');
 
 // clearing
+// console.clear();
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p);
 
 // Grouping together
+dogs.forEach(dog => {
+	console.group(`${dog.name}`);
+	console.log(`This is ${dog.name}`);
+	console.log(`${dog.name} is ${dog.age} years old`);
+	console.log(`${dog.name} is ${dog.age * 7} dog years`);
+	console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count('Bryan');
+console.count('Bryan');
+console.count('Bryan');
+console.count('Bryan');
+console.count('Bryan');
 
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/bryandaniswara')
+	.then(data => data.json())
+	.then(data => {
+		console.timeEnd('fetching data');
+		console.log(data);
+	});
+
+// table
+console.table(dogs);
